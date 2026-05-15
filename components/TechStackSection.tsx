0ef64df0163434821ef438c26ec9baaccd0644e8
Karmjeet Chauhan <karmpluswin@gmail.com>
2026-05-15 07:36:28 +0530

    added: added tech-stack section

--- components/TechStackSection.tsx
+++ components/TechStackSection.tsx
@@ -10,25 +10,45 @@ import {
 } from "@/components/ui/tooltip";
 
 export const TECH_STACK = [
-  { name: "TypeScript", icon: "/stack/typescript.svg", url: "https://www.typescriptlang.org/" },
-  { name: "JavaScript", icon: "/stack/javascript.svg", url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
+  {
+    name: "TypeScript",
+    icon: "/stack/typescript.svg",
+    url: "https://www.typescriptlang.org/",
+  },
+  {
+    name: "JavaScript",
+    icon: "/stack/javascript.svg",
+    url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+  },
   { name: "Java", icon: "/stack/java.svg", url: "https://www.java.com/" },
   { name: "Spring Boot", icon: "/stack/spring.svg", url: "https://spring.io/" },
   { name: "Node.js", icon: "/stack/nodejs.svg", url: "https://nodejs.org/" },
   { name: "Bun", icon: "/stack/bun.svg", url: "https://bun.sh/" },
   { name: "React", icon: "/stack/react.svg", url: "https://react.dev/" },
   { name: "Next.js", icon: "/stack/nextjs.svg", url: "https://nextjs.org/" },
-//   { name: "Angular", icon: "/stack/angular.svg", url: "https://angular.io/" },
-  { name: "Tailwind CSS", icon: "/stack/tailwindcss.svg", url: "https://tailwindcss.com/" },
-  { name: "Bootstrap", icon: "/stack/bootstrap.svg", url: "https://getbootstrap.com/" },
-//   { name: "shadcn/ui", icon: "/stack/shadcn-ui.svg", url: "https://ui.shadcn.com/" },
-//   { name: "Radix UI", icon: "/stack/radixui.svg", url: "https://www.radix-ui.com/" },
+  //   { name: "Angular", icon: "/stack/angular.svg", url: "https://angular.io/" },
+  {
+    name: "Tailwind CSS",
+    icon: "/stack/tailwindcss.svg",
+    url: "https://tailwindcss.com/",
+  },
+  {
+    name: "Bootstrap",
+    icon: "/stack/bootstrap.svg",
+    url: "https://getbootstrap.com/",
+  },
+  //   { name: "shadcn/ui", icon: "/stack/shadcn-ui.svg", url: "https://ui.shadcn.com/" },
+  //   { name: "Radix UI", icon: "/stack/radixui.svg", url: "https://www.radix-ui.com/" },
   { name: "Motion", icon: "/stack/motion.svg", url: "https://motion.dev/" },
   { name: "Redux", icon: "/stack/redux.svg", url: "https://redux.js.org/" },
   { name: "Git", icon: "/stack/git.svg", url: "https://git-scm.com/" },
   { name: "Docker", icon: "/stack/docker.svg", url: "https://www.docker.com/" },
   { name: "MySQL", icon: "/stack/mysql.svg", url: "https://www.mysql.com/" },
-  { name: "MongoDB", icon: "/stack/mongodb.svg", url: "https://www.mongodb.com/" },
+  {
+    name: "MongoDB",
+    icon: "/stack/mongodb.svg",
+    url: "https://www.mongodb.com/",
+  },
   { name: "Redis", icon: "/stack/redis.svg", url: "https://redis.io/" },
   { name: "Figma", icon: "/stack/figma.svg", url: "https://www.figma.com/" },
 ];
@@ -61,7 +81,10 @@ export default function TechStackSection() {
                   />
                 </Link>
               </TooltipTrigger>
-              <TooltipContent side="top" className="rounded-md bg-slate-900 dark:bg-slate-100 px-2 py-1 text-xs font-medium text-white dark:text-slate-900 shadow-lg">
+              <TooltipContent
+                side="top"
+                className="rounded-md bg-slate-900 dark:bg-slate-100 px-2 py-1 text-xs font-medium text-white dark:text-slate-900 shadow-lg"
+              >
                 {tech.name}
               </TooltipContent>
             </Tooltip>

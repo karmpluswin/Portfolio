1238c16acd1b2d353acdf7212cb57f725caaa062
KRISH <addresskrish@gmail.com>
2026-06-07 00:07:18 +0530

    feat: added 1st pro chatvibe

--- lib/projects.ts
+++ lib/projects.ts
@@ -11,11 +11,11 @@ export type Project = {
 export const PROJECTS: Project[] = [
   {
     date: "June 2026",
-    name: "Coming soon",
-    github: "#",
-    live: "#",
-    image: "/project.jpg",
-    description: "Coming soon.",
+    name: "ChatVibe",
+    github: "https://github.com/karmpluswin/ChatVibe",
+    live: "https://chatvibe-online.vercel.app/",
+    image: "/chatvibe.png",
+    description: "Real-time chat app with 1-on-1 & group messaging, emoji reactions, file sharing, and OAuth, built with Next.js 14, Socket.io, MongoDB & Framer Motion.",
     featured: true,
   },
   {
